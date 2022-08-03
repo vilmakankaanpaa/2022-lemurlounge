502d9acb3811b2c8896561c27631e90511818063
Vilma Kankaanpää <vilma.kankaanpaa@aalto.fi>
2022-08-03 11:13:41 +0100

    audioplayer volume setting

--- audioplayer.py
+++ audioplayer.py
@@ -1,27 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-#from mpyg321.mpyg321 import MPyg321Player, PlayerStatusx
 from pygame import mixer
 from time import sleep
 
-#class AudioPlayer(MPyg321Player):
 class AudioPlayer:
-
-    # Player status values:
-    # 0 - ready
-    # 1 - playing
-    # 2 - paused
-    # 3 - stopped
-    # 4 - has quit
-
-    # use self.stop(), self.pause()
 
     mixer.init()
 
     def load_audio(self, audioFile):
       mixer.music.load(audioFile)
-      mixer.music.set_volume(0.7)
+      mixer.music.set_volume(0.8)
       mixer.music.play(loops=-1)
       mixer.music.pause()
 
@@ -38,38 +27,9 @@ class AudioPlayer:
       
       return True
 
-    #     if self.status == 1: # playing
-    #         return True
-    #     else:
-    #         return False
-
-
     def has_quit(self):
       
       if mixer.get_init() == None:
         return True
       
       return False
-
-    #     if self.status == 4: # has quit
-    #         return True
-    #     else:
-    #         return False
-
-
-    #def play_audio(self, file):
-
-    #     # TODO: cannot be paused with this system
-    #     #if self.status == 2: # paused
-    #     #    self.resume()
-    #     #elif self.status != 1: # ready (0) or stopped (3)
-    #         # currently not paused and not playing -> start
-    #     self.play_song(file, loop=True)
-    #     #else:
-    #     #    pass
-
-    # #def pause(self):
-    # #    self.pause()
-
-    # #def stop(self):
-    # #    self.stop()
